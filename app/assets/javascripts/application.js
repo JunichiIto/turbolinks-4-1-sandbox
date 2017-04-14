a5336161e1343c500b1d31a75a16a58dbafaa104
Junichi Ito <jit@sonicgarden.jp>
2017-04-14 09:14:00 +0900

    Fix turbolinks issue with https://github.com/kossnocorp/jquery.turbolinks/issues/61#issuecomment-246404796

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
-//= require_tree .
+//= require jquery.turbolinks
 //= require turbolinks
+//= require turbolinks-compatibility
+//= require_tree .
